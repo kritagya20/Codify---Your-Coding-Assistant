764b1ed6576b7ec3e8c756b4d4853ae77ce5c4b1
Kritagya Singh Chouhan <kritagya2022@gmail.com>
2023-01-24 09:21:36 +0530

    updated the url

--- client/script.js
+++ client/script.js
@@ -95,7 +95,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   //fetching data from the server -> bots response
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://codify-your-coding-assistant.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
